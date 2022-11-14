37f6c14dafc370094bf60034eb12d14a4e14f19c
stn <smrcouto88@gmail.com>
2022-11-14 09:03:06 -0300

    ogimage realfavicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,6 @@ import vue from "@astrojs/vue";
 import solidJs from "@astrojs/solid-js";
 
 export default defineConfig({
+  site: "https://santuan.vercel.app/",
   integrations: [tailwind(), react(), svelte(), vue(), solidJs(), astroImageTools]
 });
