cb1ae3298605509b2f0b436d8d48cb650c508b0c
sr-couto <santuan.web@gmail.com>
2024-03-21 09:38:21 -0300

    robots txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
 
+// https://astro.build/config
 export default defineConfig({
   site: "https://astro-stn.vercel.app/",
-  integrations: [
-    tailwind(),
-    vue(),
-    prefetch(),
-    sitemap(),
-  ],
+  integrations: [tailwind(), vue(), prefetch(), sitemap(), robotsTxt()]
 });
